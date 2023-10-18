25a3afba83e1fdb2699b0f35f8ebd6b650067451
ran-earlyai <145557522+ran-earlyai@users.noreply.github.com>
2023-10-18 14:37:28 +0300

    Update hello_world.ts
    Reduce coverage.

--- src/hello_world.ts
+++ src/hello_world.ts
@@ -3,6 +3,12 @@ const greeting = 'world';
 export function hello(world: string = greeting): string {
     if (world == "Bob") {
         return "Hiya Bob!";
+    } else if (world == "Dog") {
+        return "Hiya Dog!";
+    } else if (world == "God") {
+        return "Hiya God!";
+    } else if (world == "Charlie") {
+        return "Hiya Charlie!";
     }
     return `Hello ${world}!`;
 }
